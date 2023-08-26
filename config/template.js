2af78a09d358aaeb3b93dc451c904e379dfc4d65
wjs <wangxuantask@163.com>
2023-08-26 23:13:03 +0800

    feat: 添加uniapp-vue3模板

--- config/template.js
+++ config/template.js
@@ -3,5 +3,6 @@ export const templates = {
   'vue3': 'antfu/vitesse',
   'vue3-lite': 'antfu/vitesse-lite',
   'nuxt3': 'antfu/vitesse-nuxt3',
-  'webExtension': 'antfu/vitesse-webext'
+  'webExtension': 'antfu/vitesse-webext',
+  'uniapp-vue3': 'MellowCo/uni-vue3-starter',
 };
